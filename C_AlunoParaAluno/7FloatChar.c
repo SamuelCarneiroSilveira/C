50b00e3ee8ad16316d961135d0f7f389ec23c405
SamuelCarneiroSilveira <SamuelCarneiroSilveira@gmail.com>
2022-07-09 09:28:06 -0300

    floatChar

---
+++ C_AlunoParaAluno/7FloatChar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+    int inteira = 10;
+    float decimal = 10.5;
+    char caracter = 'a';
+
+    
+    printf("%i\n", inteira);
+    printf("%f\n", decimal);
+    printf("%c\n", caracter);
+
+    return 0;
+} 
